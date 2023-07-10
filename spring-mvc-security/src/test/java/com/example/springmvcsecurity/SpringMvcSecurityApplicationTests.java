890e9beef76855511586b419db834c992fc5801d
Kalindu Agathisi <kalinduagathisi@gmail.com>
2023-07-10 22:27:03 +0530

    spring-mvc-security

---
+++ spring-mvc-security/src/test/java/com/example/springmvcsecurity/SpringMvcSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springmvcsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
